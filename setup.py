f99e65e820b58fae3e88f6814f2425ce6dda672b
ryuuzuiih <ryuuzuiih@yahoo.co.jp>
2024-08-22 21:07:19 +0900

    Initial commit of tsanalysis_pipeline

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tsanalysis_pipeline",
+    version="0.1.0",
+    package_dir={"": "src"},  
+    packages=find_packages(where="src"),  
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "statsmodels",
+        "scikit-learn",
+        "pyyaml",
+    ],
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A comprehensive time series analysis pipeline",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/tsanalysis_pipeline",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
