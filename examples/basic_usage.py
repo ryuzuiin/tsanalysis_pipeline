42eb86ae05dffe138ae9bd01e44d9e430b86c82c
ryuuzuiih <ryuuzuiih@yahoo.co.jp>
2024-08-25 21:45:12 +0900

    未归类

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,9 +1,3 @@
+import os
+import pandas as pd
 from tsanalysis_pipeline import CSVColumnExtractor, ModelingTimeSeriesAnalyzer
-
-
-if __name__ == "__main__":
-    extractor = CSVColumnExtractor('data', {'file1.csv': ['column1', 'column2']}, 'timestamp')
-    data = extractor.extract_columns()
-
-    analyzer = ModelingTimeSeriesAnalyzer(data, 'timestamp', 'value')
-    analyzer.run_modeling_analysis()
